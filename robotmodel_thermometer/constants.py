c8059a785b8dd586f5bfc502401585318dfa9315
Bart Gijsbertsen <bartgijsbertsen@gmail.com>
2021-10-22 13:21:13 +0200

    Added basic constants

---
+++ robotmodel_thermometer/constants.py
@@ -0,0 +1,11 @@
+class Constants:
+    # Environment
+    water_volume = 200 # liters
+    ambient_temperature = 15 # degrees celsius
+    start_temperature = 20 # degrees celsius
+
+    # Heater
+    heater_delta = 0.14
+
+    # Sensor
+    sensor_delta = 0.07
