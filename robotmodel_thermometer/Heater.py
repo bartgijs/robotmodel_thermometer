abbe20eaaec45575e31b2231499241af5f509f0a
PimClappers <pim.clappers@gmail.com>
2021-10-22 12:06:37 +0200

    added simple publisher

--- robotmodel_thermometer/Heater.py
+++ robotmodel_thermometer/Heater.py
@@ -1,5 +1,24 @@
+import rclpy
+from rclpy.node import Node
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(float, 'topic', 10)
+        # timer_period = 0.5  # seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
 def main():
-    print('Hi from robotmodel_thermometer : Heater')
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
